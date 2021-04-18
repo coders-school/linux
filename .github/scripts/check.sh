48128429cccf6ba524396597d53ad167445b0c98
Łukasz Ziobroń <lukasz@coders.school>
2021-04-18 16:41:35 +0200

    CI ready

---
+++ .github/scripts/check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source functions.sh
+
+FILE=../../homework/linux.txt
+fileExist ${FILE}
+
+P=processes\.txt
+C=copy\.txt
+S=selected\.txt
+M=commands\.txt
+
+echo "🔎 Performing checks"
+echo
+pattern ${FILE} "Directory created" "mkdir(\s+)"
+pattern ${FILE} "'$P' created" "touch(\s+)$P"
+pattern ${FILE} "'ps aux > $P' used" "ps(\s+)[-]?aux(\s*)>(\s*)$P"
+pattern ${FILE} "'$P' copied to '$C''" "cp(\s+)$P(\s+)$C"
+pattern ${FILE} "'backup' directory created'" "mkdir(\s+)backup"
+pattern ${FILE} "'$C' moved into 'backup' directory" "mv(\s+)$C(\s)backup|mv(\s+)\.\.\/$C(\s+)\."
+pattern ${FILE} "Lines counted and saved to '$S'" "wc(\s+)(-.*l.*)?$P(\s+)>{1,2}(\s+)$S"
+pattern ${FILE} "Grepped for '/bin' in '$P' and appended to $S" "grep(\s+)-.*n.*\/bin(\s+)$P(\s+)>>(\s+)$S"
+pattern ${FILE} "'history' with 'tail' used and saved to $M" "history(\s+)\|(\s+)tail(\s+)-n(\s*)\d+(\s+)>{1,2}(\s+)$M"
+pattern ${FILE} "'cat $S $M' used" "cat(\s+)$S(\s+)$M"
+echo
+
+summary
---
+++ .github/scripts/functions.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+RED="\e[41m"
+GREEN="\e[42m"
+BOLD="\e[1m"
+DEFAULT="\e[0m"
+
+failed=0
+total=0
+
+function check()
+{
+    (( total++ ))
+    if [ "$1" != 0 ] ; then
+        (( failed++ ))
+        echo "❌ Failed"
+        return 1
+    else
+        echo "✅ Passed"
+        return 0
+    fi
+}
+
+function pattern()
+{
+    echo -n "➡️  $2 "
+    egrep "$3" "$1" &> /dev/null
+    check $?
+    return $?
+}
+
+function compile()
+{
+    echo "💻 Compiling code..."
+    make -j
+    check $?
+    return $?
+}
+
+function fileExist()
+{
+    if [ ! -f "$1" ]; then
+        echo "❌ $1 does not exist"
+        exit -1
+    fi
+}
+
+function summary()
+{
+    if [[ "${failed}" -eq 0 ]] ; then
+        echo "🍾 All checks have passed"
+    else
+        echo "❌ ${failed}/${total} failed"
+        echo
+        echo "If you think this is a bug in the checking script please raise an Issue in this repo and describe the problem"
+    fi
+    return ${failed}
+}
